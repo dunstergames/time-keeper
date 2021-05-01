8ef59e8049ca4a8cfca272f2f3a41846dad309d2
Callum Dunster <cdunster@users.noreply.github.com>
2021-05-01 18:27:21 +0200

    Add screen shake if tick missed.

--- main.ts
+++ main.ts
@@ -2,36 +2,43 @@ namespace SpriteKind {
     export const HUD = SpriteKind.create()
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    timer.throttle("playerAction", actionInterval, function () {
-        if (!(tiles.tileIsWall(tiles.locationInDirection(tiles.locationOfSprite(playerSprite), CollisionDirection.Top)))) {
-            playerSprite.y += 0 - tiles.tileWidth()
-        }
-    })
+    if (!(tiles.tileIsWall(tiles.locationInDirection(tiles.locationOfSprite(playerSprite), CollisionDirection.Top)))) {
+        playerSprite.y += 0 - tiles.tileWidth()
+        onPlayerAction()
+    }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    timer.throttle("playerAction", actionInterval, function () {
-        if (!(tiles.tileIsWall(tiles.locationInDirection(tiles.locationOfSprite(playerSprite), CollisionDirection.Left)))) {
-            playerSprite.x += 0 - tiles.tileWidth()
-        }
-    })
+    if (!(tiles.tileIsWall(tiles.locationInDirection(tiles.locationOfSprite(playerSprite), CollisionDirection.Left)))) {
+        playerSprite.x += 0 - tiles.tileWidth()
+        onPlayerAction()
+    }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    timer.throttle("playerAction", actionInterval, function () {
-        if (!(tiles.tileIsWall(tiles.locationInDirection(tiles.locationOfSprite(playerSprite), CollisionDirection.Right)))) {
-            playerSprite.x += tiles.tileWidth()
-        }
-    })
+    if (!(tiles.tileIsWall(tiles.locationInDirection(tiles.locationOfSprite(playerSprite), CollisionDirection.Right)))) {
+        playerSprite.x += tiles.tileWidth()
+        onPlayerAction()
+    }
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    timer.throttle("playerAction", actionInterval, function () {
-        if (!(tiles.tileIsWall(tiles.locationInDirection(tiles.locationOfSprite(playerSprite), CollisionDirection.Bottom)))) {
-            playerSprite.y += tiles.tileWidth()
-        }
-    })
+    if (!(tiles.tileIsWall(tiles.locationInDirection(tiles.locationOfSprite(playerSprite), CollisionDirection.Bottom)))) {
+        playerSprite.y += tiles.tileWidth()
+        onPlayerAction()
+    }
 })
+function onPlayerAction () {
+    timeSinceTick = game.runtime() % actionInterval
+    timeUntilTick = actionInterval - timeSinceTick
+    if (timeSinceTick > gracePeriod && timeUntilTick < gracePeriod) {
+        scene.cameraShake(2, actionInterval)
+    }
+}
+let timeUntilTick = 0
+let timeSinceTick = 0
 let playerSprite: Sprite = null
+let gracePeriod = 0
 let actionInterval = 0
 actionInterval = 500
+gracePeriod = actionInterval / 2
 tiles.loadMap(tiles.createMap(tilemap`level0`))
 playerSprite = sprites.create(assets.image`Player`, SpriteKind.Player)
 tiles.placeOnRandomTile(playerSprite, sprites.dungeon.stairSouth)
