c432ede566b8c8928fb99c4d406217ac77d183ba
Callum Dunster <cdunster@users.noreply.github.com>
2021-05-01 17:47:41 +0200

    Add clock/timer animation.

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const HUD = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     timer.throttle("playerAction", actionInterval, function () {
         if (!(tiles.tileIsWall(tiles.locationInDirection(tiles.locationOfSprite(playerSprite), CollisionDirection.Top)))) {
@@ -28,8 +31,34 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 let playerSprite: Sprite = null
 let actionInterval = 0
-actionInterval = 1000
+actionInterval = 500
 tiles.loadMap(tiles.createMap(tilemap`level0`))
 playerSprite = sprites.create(assets.image`Player`, SpriteKind.Player)
 tiles.placeOnRandomTile(playerSprite, sprites.dungeon.stairSouth)
 scene.cameraFollowSprite(playerSprite)
+let timerSprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.HUD)
+timerSprite.setFlag(SpriteFlag.RelativeToCamera, true)
+timerSprite.setPosition(12, 94)
+animation.runImageAnimation(
+timerSprite,
+assets.animation`Clock`,
+actionInterval,
+true
+)
